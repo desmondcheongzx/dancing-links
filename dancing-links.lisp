8f15af7b40f046c8bbf494eec1616d8cab02995d
Desmond Cheong <desmondcheongzx@gmail.com>
2016-11-27 14:46:40 +0800

    Added matrix initialization and matrix exploration

--- dancing-links.lisp
+++ dancing-links.lisp
@@ -23,26 +23,31 @@
 
 (defvar *matrix* nil)
 
+(defun initialize-matrix (columns matrix)
+  (setf *matrix* (make-instance 'data :name 'header :columns columns))
+  (loop for row in matrix do (add-row row)))
+
 (defun add-row (row)
+  "Adds a row of data objects to *matrix*."
   (let ((leftmost nil))
     (loop
        for i in row
        for c = (first-column *matrix*) then (right c)
        do (when (= i 1)
-	  (let ((new-data (insert-data c)))
-	    (if (null leftmost)
-		(progn (setf leftmost new-data)
-		       (setf (left leftmost) leftmost)
-		       (setf (right leftmost) leftmost))
-		(progn (setf (right new-data) leftmost)
-		       (setf (left new-data) (left leftmost))
-		       (setf (right (left leftmost)) new-data)
-		       (setf (left leftmost) new-data))))))))
+	    (let ((new-data (insert-data c)))
+	      (if (null leftmost)
+		  (progn (setf leftmost new-data)
+			 (setf (left leftmost) leftmost)
+			 (setf (right leftmost) leftmost))
+		  (progn (setf (right new-data) leftmost)
+			 (setf (left new-data) (left leftmost))
+			 (setf (right (left leftmost)) new-data)
+			 (setf (left leftmost) new-data))))))))
 
-(defun first-column (matrix)
-  (right matrix))
+(defun first-column (matrix) (right matrix))
 
 (defun insert-data (column)
+  "Inserts a data object into a given column. Returns the new data object."
   (let ((new-data (make-instance 'data
 				 :up (up column)
 				 :down column
@@ -50,3 +55,20 @@
     (setf (down (up column)) new-data)
     (setf (up column) new-data)
     new-data))
+
+(defun explore (matrix)
+  (let ((cursor (first-column matrix)))
+    (loop for input = (read)
+       until (equal input 'q)
+       do (case input
+	    (a (setf cursor (left cursor)))
+	    (s (setf cursor (down cursor)))
+	    (d (setf cursor (right cursor)))
+	    (w (setf cursor (up cursor)))
+	    (p (format t "~a~%"
+		       (if (null (name cursor))
+			   (name (column cursor))
+			   (name cursor))))))))
+
+
+

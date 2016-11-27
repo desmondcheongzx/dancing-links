ab5c6dab3572f74c8e95fdc22dd53c57771fb071
Desmond Cheong <desmondcheongzx@gmail.com>
2016-11-27 14:19:41 +0800

    Added row insertion for matrices of linked data objects

--- dancing-links.lisp
+++ dancing-links.lisp
@@ -9,8 +9,44 @@
    (name :initarg :name :initform nil :accessor name)))
 
 (defmethod initialize-instance :after ((x data) &key (columns nil))
+  (setf (left x) x)
   (unless (null columns)
     (loop
        for prev = x then (right prev)
        for c in columns
-	 do (setf (right prev) (make-instance 'data :name c :right x)))))
+       do (let ((new-data (make-instance 'data :name c :right x)))
+	    (setf (up new-data) new-data)
+	    (setf (down new-data) new-data)
+	    (setf (right prev) new-data)
+	    (setf (left new-data) prev)
+	    (setf (left x) new-data)))))
+
+(defvar *matrix* nil)
+
+(defun add-row (row)
+  (let ((leftmost nil))
+    (loop
+       for i in row
+       for c = (first-column *matrix*) then (right c)
+       do (when (= i 1)
+	  (let ((new-data (insert-data c)))
+	    (if (null leftmost)
+		(progn (setf leftmost new-data)
+		       (setf (left leftmost) leftmost)
+		       (setf (right leftmost) leftmost))
+		(progn (setf (right new-data) leftmost)
+		       (setf (left new-data) (left leftmost))
+		       (setf (right (left leftmost)) new-data)
+		       (setf (left leftmost) new-data))))))))
+
+(defun first-column (matrix)
+  (right matrix))
+
+(defun insert-data (column)
+  (let ((new-data (make-instance 'data
+				 :up (up column)
+				 :down column
+				 :column column)))
+    (setf (down (up column)) new-data)
+    (setf (up column) new-data)
+    new-data))
